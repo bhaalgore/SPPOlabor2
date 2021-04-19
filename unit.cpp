8e61d38241e116c68b108a200525aa88990232b7
Anton <inamometr1@gmail.com>
2021-04-19 17:25:23 +0700

    small fix of method arguments

--- unit.cpp
+++ unit.cpp
@@ -1,6 +1,15 @@
 #include "unit.h"
 
-Unit::Unit()
+void Unit::add( const std::shared_ptr< Unit >& , Flags = 0)
 {
-
+     throw std::runtime_error("Not supported");
 }
+
+std::string Unit::generateShift( unsigned int level ) const {
+ static const auto DEFAULT_SHIFT = " ";
+ std::string result;
+ for( unsigned int i = 0; i < level; ++i ) {
+ result += DEFAULT_SHIFT;
+ }
+ return result;
+ }
--- unit.h
+++ unit.h
@@ -1,11 +1,24 @@
 #ifndef UNIT_H
 #define UNIT_H
 
+#include <iostream>
+#include <memory>
 
-class Unit
-{
+
+class Unit {
 public:
-    Unit();
+ using Flags = unsigned int;
+
+public:
+ virtual ~Unit() = default;
+
+ virtual void add( const std::shared_ptr< Unit >& , Flags );
+
+ virtual std::string compile( unsigned int level = 0 ) const = 0;
+
+protected:
+ virtual std::string generateShift( unsigned int level ) const = 0;
+
 };
 
 #endif // UNIT_H
