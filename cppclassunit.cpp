cf2ab1942fae9f85ef57dcc8d4e9aae1c98c061a
Anton <inamometr1@gmail.com>
2021-04-19 17:17:26 +0700

    Created classes  cppClassUnit and Unit

---
+++ cppclassunit.cpp
@@ -0,0 +1,6 @@
+#include "cppclassunit.h"
+
+cppClassUnit::cppClassUnit()
+{
+
+}
---
+++ cppclassunit.h
@@ -0,0 +1,11 @@
+#ifndef CPPCLASSUNIT_H
+#define CPPCLASSUNIT_H
+
+
+class cppClassUnit
+{
+public:
+    cppClassUnit();
+};
+
+#endif // CPPCLASSUNIT_H
