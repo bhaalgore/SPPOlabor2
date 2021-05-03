bd9c0fcbfb5eacee45934e0b21037556ce9e701f
Anton <inamometr1@gmail.com>
2021-05-03 09:11:18 +0700

    Created class csharpClassUnit

---
+++ csharpclassunit.cpp
@@ -0,0 +1,6 @@
+#include "csharpclassunit.h"
+
+csharpClassUnit::csharpClassUnit()
+{
+
+}
---
+++ csharpclassunit.h
@@ -0,0 +1,11 @@
+#ifndef CSHARPCLASSUNIT_H
+#define CSHARPCLASSUNIT_H
+
+
+class csharpClassUnit
+{
+public:
+    csharpClassUnit();
+};
+
+#endif // CSHARPCLASSUNIT_H
