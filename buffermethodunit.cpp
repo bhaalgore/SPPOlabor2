c241407e22545dc862d3343a8d1cd208312a0de5
Anton <inamometr1@gmail.com>
2021-05-17 11:41:36 +0700

    Created class bufferMethodUnit

---
+++ buffermethodunit.cpp
@@ -0,0 +1,6 @@
+#include "buffermethodunit.h"
+
+bufferMethodUnit::bufferMethodUnit()
+{
+
+}
---
+++ buffermethodunit.h
@@ -0,0 +1,11 @@
+#ifndef BUFFERMETHODUNIT_H
+#define BUFFERMETHODUNIT_H
+
+
+class bufferMethodUnit
+{
+public:
+    bufferMethodUnit();
+};
+
+#endif // BUFFERMETHODUNIT_H
