5442667a6afb422a0a46462b33bff863fc98c593
Anton <inamometr1@gmail.com>
2021-05-17 10:03:56 +0700

    Created class bufferclassunit

---
+++ bufferclassunit.cpp
@@ -0,0 +1,6 @@
+#include "bufferclassunit.h"
+
+bufferClassUnit::bufferClassUnit()
+{
+
+}
---
+++ bufferclassunit.h
@@ -0,0 +1,11 @@
+#ifndef BUFFERCLASSUNIT_H
+#define BUFFERCLASSUNIT_H
+
+
+class bufferClassUnit
+{
+public:
+    bufferClassUnit();
+};
+
+#endif // BUFFERCLASSUNIT_H
